1f2d61e70c92132bdc5fe3be49185456f24c4ca4
Jamshid <jamshidjabbarov17@gmail.com>
2022-05-26 17:31:54 +0500

    modified admin.py

--- main/views.py
+++ main/views.py
@@ -13,9 +13,15 @@ def main(request):
                 text="postdan o'tdik")
         form = UserInfoForm(request.POST)
         if form.is_valid():
+            bot.send_message(chat_id=366321052, 
+                text="validdan o'tdik")
             name = form.cleaned_data['name']
             phone_number = form.cleaned_data['phone_number']
-            client = Client.objects.create(name=name, phone_number=phone_number)
+            try:
+                client = Client.objects.create(name=name, phone_number=phone_number)
+            except Exception as e:
+                bot.send_message(chat_id=366321052, 
+                    text=f"exception: {e}")
             bot.send_message(chat_id=366321052, 
                 text=f"Mijoz ismi: {client.name}\nTelefon raqami: {client.phone_number}\nBuyurtma vaqti: {client.request_time}")
 

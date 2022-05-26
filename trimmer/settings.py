07e29ce960de6a518090f2b87859be4b6110db1a
Jamshid <jamshidjabbarov17@gmail.com>
2022-05-26 17:10:41 +0500

    final finally

--- trimmer/settings.py
+++ trimmer/settings.py
@@ -27,8 +27,8 @@ BOT_API_KEY = "5351634212:AAE0vkBX2kC4pE45MobkW43y0iD6SxTzTQE"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
+CSRF_TRUSTED_ORIGINS = ["trimmer-shopping.herokuapp.com"]
 ALLOWED_HOSTS = ["https://trimmer-shopping.herokuapp.com/"]
-CSRF_TRUSTED_ORIGINS = ["https://trimmer-shopping.herokuapp.com/"]
 
 
 # Application definition

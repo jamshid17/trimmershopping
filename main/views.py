e4e1fc31c644f334d169374b130ec21688658877
Jamshid <jamshidjabbarov17@gmail.com>
2022-05-26 17:16:09 +0500

    funeral

--- main/views.py
+++ main/views.py
@@ -15,10 +15,11 @@ def main(request):
         if form.is_valid():
             name = form.cleaned_data['name']
             phone_number = form.cleaned_data['phone_number']
-            bot.send_message(chat_id=366321052, 
-                text=f"Mijoz ismi: {client.name}\nTelefon raqami: {client.phone_number}\nBuyurtma vaqti: {client.request_time}")
             client = Client.objects.create(name=name, phone_number=phone_number)
             client.save()
+            bot.send_message(chat_id=366321052, 
+                text=f"Mijoz ismi: {client.name}\nTelefon raqami: {client.phone_number}\nBuyurtma vaqti: {client.request_time}")
+
         else:
             error_message = "Insert correct type of phone number"
         return render(request, "main/index.html", context={"form":UserInfoForm})
